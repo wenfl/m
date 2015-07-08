1804d93469032dd6a34fe3eb6259a92cfac088b2
wenfl <wenfl@email.com>
2015-07-08 18:05:17 +0800

    m

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/m/admin/');
+define('HTTP_CATALOG', 'http://localhost/m/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/m/admin/');
+define('HTTPS_CATALOG', 'http://localhost/m/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/wamp/www/m/admin/');
+define('DIR_SYSTEM', 'D:/wamp/www/m/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/m/admin/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/m/admin/view/template/');
+define('DIR_CONFIG', 'D:/wamp/www/m/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/m/image/');
+define('DIR_CACHE', 'D:/wamp/www/m/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/m/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/m/system/upload/');
+define('DIR_LOGS', 'D:/wamp/www/m/system/logs/');
+define('DIR_MODIFICATION', 'D:/wamp/www/m/system/modification/');
+define('DIR_CATALOG', 'D:/wamp/www/m/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/m/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/m/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/wamp/www/m/catalog/');
+define('DIR_SYSTEM', 'D:/wamp/www/m/system/');
+define('DIR_LANGUAGE', 'D:/wamp/www/m/catalog/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/m/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/wamp/www/m/system/config/');
+define('DIR_IMAGE', 'D:/wamp/www/m/image/');
+define('DIR_CACHE', 'D:/wamp/www/m/system/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/m/system/download/');
+define('DIR_UPLOAD', 'D:/wamp/www/m/system/upload/');
+define('DIR_MODIFICATION', 'D:/wamp/www/m/system/modification/');
+define('DIR_LOGS', 'D:/wamp/www/m/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
